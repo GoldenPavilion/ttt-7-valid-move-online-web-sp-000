185e43485839a5005887e8b9215894208761f831
Robert Stine <rcorystine@gmail.com>
2020-12-09 17:34:41 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
-# code your #valid_move? method here
-
+def valid_move?(board, index)
+end
 
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
